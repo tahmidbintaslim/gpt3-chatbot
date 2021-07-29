ae0c776c07ea8e515a01d5bcffe7d7f8ca77631d
Rafi <tahmidbintaslimrafi@gmail.com>
2021-07-29 19:03:44 +0700

    chnages

--- chefbot.py
+++ chefbot.py
@@ -1,22 +1,23 @@
+from chefbot import append_interaction_to_chat_log, ask
 from flask import Flask, request, session
-from ava import append_interaction_to_chat_log, ask
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
-# if for some reason your conversation with the bot gets weird, change the secret key 
-app.config['SECRET_KEY'] = '89djhff9lhkd93'
+# if for some reason, your conversation with the chef gets weird, change the secret key 
+app.config['SECRET_KEY'] = 'top-NSFJSFDKFjkfsk!'
 
-@app.route('/ava', methods=['POST'])
-def ava():
+@app.route('/chefbot', methods=['POST'])
+def chef():
     incoming_msg = request.values['Body']
     chat_log = session.get('chat_log')
     answer = ask(incoming_msg, chat_log)
     session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
                                                          chat_log)
+    # print("the session chat_log = ", chat_log)
+
     msg = MessagingResponse()
     msg.message(answer)
     return str(msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
- 
